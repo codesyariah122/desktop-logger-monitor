430583a84fbee2183c7c2e3f2aa2b459465388db
codesyariah122 <pujiermanto@gmail.com>
2025-01-10 16:31:01 +0700

    py/start

--- test-ip.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-def get_local_ip():
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    return local_ip
-
-print("Local IP Address:", get_local_ip())
